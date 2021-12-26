c6a660262f6f5298963af6ae4b1e57c1dfe2ba3e
vatimetou ebeker <fatimaabcar2@gmail.com>
2021-12-26 17:49:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Follow along with the examples here
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  sayHelloToGuadalupe();
+
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  sayHelloToLiz();
+
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  sayHelloToSamip();
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Goodbye", "Julio");
+  say("Julio", "hello");
+
+  
+  function add(x, y) {
+    return x + y;
+  }
+  console.log(add(1, 2));
+  console.log(add(80, 9000));
+
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+  console.log(say("Hello", "Liz"));
+
+  function sayHello(firstName, lastName) {
+    console.log("linda patrick");
+    return `${firstName}, ${lastName}!`;
+  }
+
+  function sayHi(Name){
+
+    console.log("mia");
+    
+
+  }
+  sayHi();
+
+  
